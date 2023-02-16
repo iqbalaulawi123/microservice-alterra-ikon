c1f3d0a012fe964f09685a87af8e437a58f58ba4
iqbalaulawi123 <iqbalaulawi123@gmail.com>
2023-02-16 11:33:16 +0700

    test

---
+++ authentication-service/src/main/java/com/iqbalaulawi/authenticationservice/entity/User.java
@@ -0,0 +1,44 @@
+package com.iqbalaulawi.authenticationservice.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users_entity")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String role;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ authentication-service/src/test/java/com/iqbalaulawi/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqbalaulawi.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
